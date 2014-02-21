2d0cb9870c6748f67ef6a7492e27a96a43f76d75
yuvaloni <yuvon2@gmail.com>
2014-02-21 17:36:58 +0200

    upload added
    upload added

---
+++ BeatLive/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="BeatLive.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Name:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Artist:<asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        Song file:<asp:FileUpload  ID="FileUpload1" runat="server" />
+    
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ BeatLive/WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+namespace BeatLive
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection C;
+        Random rnd = new Random();
+        string s = "0aA1bB2cC3dD4eE5fF6gG7hH8iI9jJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            C = new SqlConnection("Server=d968f873-9ef9-4654-aabd-a2d80114724d.sqlserver.sequelizer.com;Database=dbd968f8739ef94654aabda2d80114724d;User ID=svthayatirbfcwpp;Password=VJLZpWcGhYgbhYj4dC3gnQ5hTuQS6uzCMgyp52c4Wojj28NCDgiKaBxeMCf4fsKV;");
+            C.Open();
+
+            
+            
+        }
+        
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            string f = "";
+            bool match = false;
+            while (match)
+            {
+
+                for (int i = 0; i < 5; i++)
+                    f += s[rnd.Next(0, s.Length + 1)];
+                f += ".mp3";
+                SqlCommand t = new SqlCommand("Select * from music", C);
+                SqlDataReader r = t.ExecuteReader();
+                while(r.Read())
+                {
+                    if (r[3] == f)
+                        match = false;
+                }
+            }
+            SqlCommand u = new SqlCommand("Insert into music values (@n,@a,@f)", C);
+            u.Parameters.Add(new SqlParameter("@n", TextBox1.Text));
+            u.Parameters.Add(new SqlParameter("@a", TextBox2.Text));
+            u.Parameters.Add(new SqlParameter("@f", f));
+            u.ExecuteNonQuery();
+            File.WriteAllBytes(Path.Combine(Server.MapPath("~"),"Music",f), FileUpload1.FileBytes);
+
+        }
+    }
+}
