269d559f97d058a5904fa563bb6c51f2e1de9bfc
yuvaloni <yuvon2@gmail.com>
2014-02-21 18:38:43 +0200

    CREATE MUSIC DIRECTORY

---
+++ BeatLive/Music.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace BeatLive
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(Path.Combine(Server.MapPath("~"),"Music");
+            Response.Write("SUCCESS");
+        }
+    }
+}
--- BeatLive/Upload.aspx
+++ BeatLive/Upload.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="BeatLive.WebForm1" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="BeatLive.WebForm1" %>
 
 <!DOCTYPE html>
 
