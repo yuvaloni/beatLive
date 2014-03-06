99a512250a9dd8be1f60814d146c3bcc2f6a54a2
yuvaloni <yuvon2@gmail.com>
2014-03-06 18:06:26 +0200

    GOogle Analytics

---
+++ DJ.LIVE/CPANEL.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace DJ.LIVE
+
+{
+    public partial class CPANEL : System.Web.UI.Page
+    {
+        int port;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["p"] == null)
+            {
+                Random rnd = new Random();
+                port = rnd.Next(12345, 54322);
+                Session["p"] = port;
+            }
+            port = (int)Session["p"];
+            Response.Write(port);
+
+
+        }
+        public void Send()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            s.SendTo(ASCIIEncoding.ASCII.GetBytes("Imashca"),new IPEndPoint(IPAddress.Parse("127.0.0.1"),port));
+          
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+    }
+}
---
+++ DJ.LIVE/Listen.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace DJ.LIVE
+{
+    public partial class Listen : System.Web.UI.Page
+    {
+        static Socket l;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            l = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            l.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), int.Parse(Request.QueryString["p"])));
+            listen();
+        }
+        public void listen()
+        {
+            byte[] buffer = new byte[1024];
+            l.Receive(buffer);
+            Response.Write(ASCIIEncoding.ASCII.GetString(buffer));
+            
+        }
+    }
+}
